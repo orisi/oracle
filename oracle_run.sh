4ae88ddba708eb93d6076b433ed008ee592246a5
Kuba Kucharski <kuba.kucharski@gmail.com>
2014-07-07 14:29:15 +0200

    Update oracle_run.sh

--- oracle_run.sh
+++ oracle_run.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-/usr/sbin/sshd -D
-
-
 
 PYTHON_EXEC=python2.7
 HOME="/root"
 DIR="/root"
 
-python PyBitmessage/src/bitmessagemain.py > /dev/null &
-
-if [ -z $(pgrep bitcoind) ]
-then
-    $HOME/bitcoin/bin/$(getconf LONG_BIT)/bitcoind -connect=127.0.0.1 -datadir=$HOME/.bitcoin/ -rpcport=2521 &
-    sleep 2
-fi
-
-echo "running oracle"
 
 $PYTHON_EXEC $DIR/orisi/src/run_oracle.py $@
