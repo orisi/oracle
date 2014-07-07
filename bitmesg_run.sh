9412eb408ac1226501747b2acace9043824aed87
Kuba Kucharski <kuba.kucharski@gmail.com>
2014-07-07 14:28:41 +0200

    Update bitmesg_run.sh

--- bitmesg_run.sh
+++ bitmesg_run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-/usr/sbin/sshd -D
-
-
 
 PYTHON_EXEC=python2.7
 HOME="/root"
@@ -10,12 +7,3 @@ DIR="/root"
 
 python PyBitmessage/src/bitmessagemain.py > /dev/null &
 
-if [ -z $(pgrep bitcoind) ]
-then
-    $HOME/bitcoin/bin/$(getconf LONG_BIT)/bitcoind -connect=127.0.0.1 -datadir=$HOME/.bitcoin/ -rpcport=2521 &
-    sleep 2
-fi
-
-echo "running oracle"
-
-$PYTHON_EXEC $DIR/orisi/src/run_oracle.py $@
