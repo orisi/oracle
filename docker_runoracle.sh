aa074ca740415dccec42655f7d5de1a44e518d0c
Kuba Kucharski <kuba.kucharski@gmail.com>
2014-07-07 13:53:47 +0200

    Create docker_runoracle.sh

---
+++ docker_runoracle.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+/usr/sbin/sshd -D
+
+
+
+PYTHON_EXEC=python2.7
+HOME="/root"
+DIR="/root"
+
+screen -d - m python PyBitmessage/src/bitmessagemain.py 
+
+if [ -z $(pgrep bitcoind) ]
+then
+    $HOME/bitcoin/bin/$(getconf LONG_BIT)/bitcoind -connect=127.0.0.1 -datadir=$HOME/.bitcoin/ -rpcport=2521 &
+    sleep 2
+fi
+
+echo "running oracle"
+
+$PYTHON_EXEC $DIR/src/run_oracle.py $@
