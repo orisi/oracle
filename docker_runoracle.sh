36a1b2e3141468dea4276ae5153860eac62af18b
Kuba Kucharski <kuba.kucharski@gmail.com>
2014-07-08 10:07:16 +0200

    Update docker_runoracle.sh

--- docker_runoracle.sh
+++ docker_runoracle.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 /usr/sbin/sshd -D
-$HOME/zenoracles/initial_bitmesg_run.sh 
+bash $HOME/zenoracles/initial_bitmesg_run.sh 
 sleep 10
-$HOME/zenoracles/initial_bitcoind_run.sh
+bash $HOME/zenoracles/initial_bitcoind_run.sh
 sleep 2
-$HOME/zenoracles/initial_oracle_run.sh
+bash $HOME/zenoracles/initial_oracle_run.sh
 
