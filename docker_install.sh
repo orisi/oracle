ed9e8d239b45bba0f88f679cf1751f81b2dcf6bd
Kuba Kucharski <kuba.kucharski@gmail.com>
2014-08-28 19:13:52 +0200

    No more BitMessage.

--- docker_install.sh
+++ docker_install.sh
@@ -14,7 +14,7 @@ tar -C $HOME -zxvf $HOME/bitcoin-0.9.1-linux.tar.gz
 mv $HOME/bitcoin-0.9.1-linux $HOME/bitcoin
 rm $HOME/bitcoin-0.9.1-linux.tar.gz
 
-git clone git://github.com/Bitmessage/PyBitmessage.git $HOME/PyBitmessage
+
 
 cp $DIR/src/settings_local.py.example $DIR/src/settings_local.py
 
@@ -26,18 +26,3 @@ echo rpcuser=bitrpc >> $HOME/.bitcoin/bitcoin.conf
 echo rpcpassword=$BTCRPC >> $HOME/.bitcoin/bitcoin.conf
 echo BITCOIND_RPC_PASSWORD = \"$BTCRPC\" >> $DIR/src/settings_local.py
 
-$PYTHON_EXEC $HOME/PyBitmessage/src/bitmessagemain.py > /dev/null &
-sleep 5
-pkill -x $PYTHON_EXEC
-
-echo daemon = true >> $HOME/.config/PyBitmessage/keys.dat
-echo apienabled = true >> $HOME/.config/PyBitmessage/keys.dat
-echo apiport = 2523 >> $HOME/.config/PyBitmessage/keys.dat
-echo apiinterface = 127.0.0.1 >> $HOME/.config/PyBitmessage/keys.dat
-echo apiusername = bitrpc >> $HOME/.config/PyBitmessage/keys.dat
-#echo defaultnoncetrialsperbyte = 40 >> $HOME/.config/PyBitmessage/keys.dat
-
-
-BMPW=`openssl rand -hex 32`
-echo "apipassword = $BMPW" >> $HOME/.config/PyBitmessage/keys.dat
-echo BITMESSAGE_PASSWORD = \"$BMPW\" >> $DIR/src/settings_local.py
